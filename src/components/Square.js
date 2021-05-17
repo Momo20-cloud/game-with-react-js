5bd808f22ab00e9227e70d6a229186e9542802d3
Momo20-Cloud <mohamedjichidev@gmail.com>
2021-05-17 10:29:59 +0000

    deployment

---
+++ src/components/Square.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import reactDom from 'react-dom';
+
+
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+
+  export default Square
