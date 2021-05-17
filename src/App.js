528c390299744f6eafc859d06c1c868373438e68
Momo20-Cloud <mohamedjichidev@gmail.com>
2021-05-17 09:56:24 +0000

    deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import Square from './components/Square';
+import Board from './components/Board';
+import Game from './components/Game';
 
 function App() {
   return (
